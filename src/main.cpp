44f5318d239056e972181cbb7f9e38f68da96146
redish101 <i@redish101.top>
2026-02-20 11:10:30 +0800

    fix: 补全windows平台缺失的`WinMain`

--- src/main.cpp
+++ src/main.cpp
@@ -11,3 +11,16 @@ int main(int argc, char *argv[]) {
 
   engine.run();
 }
+
+#ifdef _WIN32
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow) {
+  int argc = __argc;
+  char **argv = __argv;
+
+  int ret = main(argc, argv);
+
+  return ret;
+}
+#endif
